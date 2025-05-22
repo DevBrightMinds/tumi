bf402b0635711d3e9a5e99213cdc610fe0acb4a4
TreasureMK <147389909+TreasureMK@users.noreply.github.com>
2025-05-22 14:40:58 +0200

    adding comments for where validation is required

--- src/api/controllers/FileUploadController.ts
+++ src/api/controllers/FileUploadController.ts
@@ -8,15 +8,16 @@ export const FileUploadController: Router = express.Router();
 const FileUploadRepo: FileUploadRepository = new FileUploadRepository()
 
 
-FileUploadController.get("/getDocument", async (req: Request, res: Response) => {
+FileUploadController.get("/get", async (req: Request, res: Response) => {
     res.send(await FileUploadRepo.get(req.body?.id));
 });
 
-FileUploadController.get("/getAllDocuments", async (_: Request, res: Response) => {
+FileUploadController.get("/getAll", async (_: Request, res: Response) => {
     res.send(await FileUploadRepo.getAll());
 });
 
 FileUploadController.post("/create", async (req: Request, res: Response) => {
+    // needs validation before uploading - there's already a validation class inside application directory 
     const { HandleFileUpload } = FileUpload();
 
     const response = await FileUploadRepo.create(req?.body);
@@ -30,9 +31,11 @@ FileUploadController.post("/create", async (req: Request, res: Response) => {
 });
 
 FileUploadController.patch("/update", async (req: Request, res: Response) => {
+    // needs validation before uploading - there's already a validation class inside application directory 
     res.send(await FileUploadRepo.update(req?.body));
 });
 
 FileUploadController.delete("/delete", async (req: Request, res: Response) => {
+    // needs validation before uploading - there's already a validation class inside application directory 
     res.send(await FileUploadRepo.delete(req?.body));
 });
